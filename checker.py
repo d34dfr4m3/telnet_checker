64e8351e0fbdb25fa21c1013f66c0f0f90092504
d34dfr4m3 <32407502+d34dfr4m3@users.noreply.github.com>
2020-01-25 21:13:49 -0300

    Update checker.py

--- checker.py
+++ checker.py
@@ -14,32 +14,32 @@ info=open('creds','r')
 timeout=2
 log=open('log','a')
 for line in info:
-  try:
+  payload=line.split(':')
+  if len(payload) == 3:
     HOST=line.split(':')[0]
     user=line.split(':')[1]
     password=line.split(':')[2].replace('\n','')
-  except Exception as err: 
-    print("[!] Error: %s" %(err))
-    continue
-  if check_port(HOST) == 0:
-    string="[!] Hostname: %s is UP." %(HOST)
-    print("\t[-] Testing Username:%s Password:%s" %(user,password))
-    try: 
-      tn=telnetlib.Telnet(HOST,timeout)
-      print('login')
-      tn.read_until("login: ")
-      tn.write(user + "\n")
-      print('password')
-      tn.read_until("Password: ")
-      tn.write(password + "\n")
-      print('command')
-      tn.write("id"+'\n')
-      tn.write("exit\n")
-      log.write(string+'\n')
-      print(tn.read_all())
-    except Exception as error: 
-      print("Error: %s" %(error))
-      continue
-  else: 
-    pass
+    if check_port(HOST) == 0:
+      string="[!] Hostname: %s is UP." %(HOST)
+      print("\t[-] Testing Username:%s Password:%s" %(user,password))
+      try: 
+        tn=telnetlib.Telnet(HOST,timeout)
+        print('login')
+        tn.read_until("login: ")
+        tn.write(user + "\n")
+        print('password')
+        tn.read_until("Password: ")
+        tn.write(password + "\n")
+        print('command')
+        tn.write("id"+'\n')
+        tn.write("exit\n")
+        log.write(string+'\n')
+        print(tn.read_all())
+      except Exception as error: 
+        print("Error: %s" %(error))
+        continue
+    else: 
 #    print("[!] %s 23/TCP CLOSED" %(HOST))
+      pass
+  else:
+    pass
